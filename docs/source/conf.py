8e296e77f96f70b5dc9da07c87a4b7c025a5ce6e
Reagan McKinney <rmckinney@a23-150.eos.ubc.ca>
2022-07-21 15:37:44 -0700

    add api

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Wfrt Deom'
+project = 'WFRT Demo'
 copyright = '2022, Reagan McKinney'
 author = 'Reagan McKinney'
 
@@ -28,7 +28,7 @@ author = 'Reagan McKinney'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = [    
     "sphinx.ext.mathjax",
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
@@ -38,20 +38,21 @@ extensions = [
     "myst_parser",
     "sphinx_copybutton",
 ]
-
 # use language set by highlight directive if no language is set by role
 inline_highlight_respect_highlight = False
 
 # use language set by highlight directive if no role is set
 inline_highlight_literals = False
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# The root document.
+root_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
