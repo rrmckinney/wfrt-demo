a0e42fde1e36ddde90f8e42baaa5501c1c340c48
Reagan McKinney <rmckinney@a23-150.eos.ubc.ca>
2022-07-21 15:11:50 -0700

    added theme to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,16 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'Wfrt Deom'
-copyright = '2022, Chris Rodell'
-author = 'Chris Rodell'
+copyright = '2022, Reagan McKinney'
+author = 'Reagan McKinney'
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,7 +28,21 @@ author = 'Chris Rodell'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosectionlabel",
+    "nbsphinx",
+    "myst_parser",
+    "sphinx_copybutton",
 ]
+
+# use language set by highlight directive if no language is set by role
+inline_highlight_respect_highlight = False
+
+# use language set by highlight directive if no role is set
+inline_highlight_literals = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,7 +58,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
